169085262b54cfbdb52c598b73334e4373431486
oaky <federicorojkin@gmail.com>
2013-07-25 17:11:16 -0300

    3do commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Roma::Application.config.secret_key_base = '8cdaea97d2440805531ece6108f0e8c1a84672aaa323c9589f62e7f9a901488a285141970505537fabe4663d641862b22f4a0ca51884783254d15a66c38eea66'
